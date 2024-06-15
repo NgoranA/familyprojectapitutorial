9495c1b1933d8b82623b63bea4802e551ce5bf94
ngorana <ngoranaristide4@gmail.com>
2024-06-15 15:43:57 +0100

    configured env variables

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,12 +1,14 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "aristide",
-  host: "localhost",
-  database: "familytree",
-  password: "password1234",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool

c3a29b086a4be959201f8dc443d48e6cda66730e
ngorana <ngoranaristide4@gmail.com>
2024-06-15 15:07:26 +0100

    Creating a user proper error management and checking if user already exists

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import pool from "../config/dbconfig.js";
 import validateFamilyData from "../utils/dataValidation.js";
+import validateParams from "../utils/validateParams.js";
 
 const router = Router();
 
@@ -13,8 +14,9 @@ router.get("/", function (req, res, next) {
       }
       if (result.rows.length < 1) {
         res.json({ message: "There are no family members in the database" });
+      } else {
+        res.json(result.rows);
       }
-      res.json(result.rows);
     });
   } catch (error) {
     next(error);
@@ -27,15 +29,65 @@ router.post("/", function (req, res, next) {
     validateFamilyData(req.body);
     const { name, age, email, sex, job_title, salary, address } = req.body;
     pool.query(
-      "INSERT INTO family_tree (name, email, age, sex, job_title, salary, address) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-      [name, email, age, sex, job_title, salary, address],
-      (err, results) => {
+      "select id from family_tree where email=$1",
+      [email],
+      (err, result) => {
         if (err) {
           throw err;
         }
-        res.status(201).json(results);
+        console.log(result.rows.length);
+        if (result.rows.length === 0) {
+          pool.query(
+            "INSERT INTO family_tree (name, email, age, sex, job_title, salary, address) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+            [name, email, age, sex, job_title, salary, address],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              res.status(201).json(results);
+            },
+          );
+        } else {
+          const err = new Error(
+            `Family Member with email : ${email} already exists`,
+          );
+          err.status = 409;
+          next(err);
+        }
       },
     );
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* delete family member */
+
+router.delete("/:id", function (req, res, next) {
+  const { id } = req.params;
+  try {
+    validateParams(+id);
+    pool.query(`select * from family_tree where id=$1`, [id], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      if (result.rowCount === 1) {
+        pool.query(
+          "delete from family_tree where id=$1",
+          [result.rows[0].id],
+          (err) => {
+            if (err) {
+              throw err;
+            }
+            res.status(204).json({ message: "family member deleted" });
+          },
+        );
+      } else {
+        const err = new Error("Element does not exist");
+        err.status = 404;
+        throw err;
+      }
+    });
   } catch (error) {
     next(error);
   }

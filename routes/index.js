aa640d8b8f075963dc5011bac96dd5229e158789
ngorana <ngoranaristide4@gmail.com>
2024-06-15 14:07:10 +0100

    creating family members implemented

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,39 @@ const router = Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  let familyMembers = [];
-  pool
-    .query("select * from family_tree;")
-    .then((family) => {
-      familyMembers = family;
-    })
-    .catch((err) => {
-      next(err);
+  try {
+    pool.query("select * from family_tree;", (err, result) => {
+      if (err) {
+        throw err;
+      }
+      if (result.rows.length < 1) {
+        res.json({ message: "There are no family members in the database" });
+      }
+      res.json(result.rows);
     });
-  if (familyMembers.length === 0) {
-    res.json({ message: "There's no family member in the database" });
+  } catch (error) {
+    next(error);
   }
-  res.json(familyMembers);
 });
 
 /* Create family members */
 router.post("/", function (req, res, next) {
-  next(validateFamilyData(req.body));
-  res.send(req.body);
+  try {
+    validateFamilyData(req.body);
+    const { name, age, email, sex, job_title, salary, address } = req.body;
+    pool.query(
+      "INSERT INTO family_tree (name, email, age, sex, job_title, salary, address) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [name, email, age, sex, job_title, salary, address],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        res.status(201).json(results);
+      },
+    );
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
